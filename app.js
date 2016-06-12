db54aa9d61c65fd36fcd57c8935c0c2cdb94c411
mattdriscoll <mdriscoll815@gmail.com>
2016-06-12 17:52:26 +0000

    actually updated dburl

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://matt:ellie@ds013574.mlab.com:13574/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://matt:ellie@ds013574.mlab.com:13574/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
